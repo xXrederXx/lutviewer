0a646c51a61c8834c81b0044d2fa50e90a8a866f
xXrederXx <morgenthalerthierry@gmail.com>
2026-01-25 17:13:50 +0100

    fix: bring back accidental delete

---
+++ engine/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using CommandLine;
+using LutViewer.Engine.Common;
+using LutViewer.Engine.Util;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.ColorSpaces.Conversion;
+
+namespace LutViewer.Engine;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<AppArgs>(args).WithParsed(appArgs => { });
+
+        Lut lut = CubeUtil.ReadCubeFile(@"C:\Users\Thierry\DEV\cross\lutviewer\test\1\Lut.cube");
+        Image<PixelFormat> img = ImageUtil.OpenImage(@"C:\Users\Thierry\DEV\cross\lutviewer\test\1\WithoutLut.JPG");
+        LutUtil.ApplyLut(img, lut);
+        img.SaveAsPng("processed.png");
+    }
+}
