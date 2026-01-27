97fc6650de0849934fb9495d7f616d64732bc5d5
xXrederXx <morgenthalerthierry@gmail.com>
2026-01-27 20:32:22 +0100

    feat: header gui layout created

--- ui/appHeader.py
+++ ui/appHeader.py
@@ -12,3 +12,13 @@ class AppHeader(ctk.CTkFrame):
 
         self.title = ctk.CTkLabel(self, text=APP_NAME, font=HEADER_FONT, height=60)
         self.title.grid(row=0, column=0, sticky="w", padx=12)
+        
+        
+        self.image_btn = ctk.CTkButton(self, text="Open Image")
+        self.image_btn.grid(row=0, column=1, padx=12)
+        
+        self.lut_btn = ctk.CTkButton(self, text="Open LUTs")
+        self.lut_btn.grid(row=0, column=2, padx=12)
+        
+        self.preview_size_dropdown = ctk.CTkOptionMenu(self, values=["Auto", "Full", "Half", "Quater", "Eigth"])
+        self.preview_size_dropdown.grid(row=0, column=3, padx=12)
